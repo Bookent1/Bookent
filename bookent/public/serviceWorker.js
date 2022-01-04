0b2c4119c0f24c055b9423bf2fbb74d458404269
Bookent1 <92177857+Bookent1@users.noreply.github.com>
2022-01-04 20:12:46 +0200

    Update serviceWorker.js

--- bookent/public/serviceWorker.js
+++ bookent/public/serviceWorker.js
@@ -1,78 +1,72 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v1';
 const assets = [
   './',
-  'index.html', 
+  'index.html',
   'fallback.html',
   'https://fonts.googleapis.com/css2?family=NTR&family=Offside&display=swap',
   'assets/bookent logo black.svg',
-  'assets/logo small.svg'
+  'assets/logo small.svg',
+  'assets/add.svg',
+  'assets/Group 18.svg',
+  'assets/home.svg',
+  'assets/love.svg',
+  'assets/lover.svg',
+  'assets/user.svg',
+  'assets/sort.svg',
+  'assets/delete.svg',
+  '/static/js/main.chunk.js',
+  '/static/js/5.chunk.js',
+  '/static/js/12.chunk.js',
+  '/static/js/13.chunk.js',
+  '/static/js/14.chunk.js',
+  '/static/js/10.chunk.js',
+  '/static/js/19.chunk.js',
+  '/static/js/20.chunk.js',
+  '/static/js/11.chunk.js',
 ];
 const self = this;
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
 
 
-
-// install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+// Install SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(assets);
+      })
+  )
 });
 
 
+// Activate the SW
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(staticCacheName);
 
-// activate event
-self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+  event.waitUntil(
+      caches.keys().then((cacheNames) => Promise.all(
+          cacheNames.map((cacheName) => {
+              if(!cacheWhitelist.includes(cacheName)) {
+                  return caches.delete(cacheName);
+              }
+          })
+      ))
+          
+  )
 });
 
 
 
 
-
-// fetch event
-self.addEventListener('fetch', evt => {
-  // check if request is made by chrome extensions or web page
-  // if request is made for web page url must contains http.
-  if (!(evt.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 50);
-          return fetchRes;
-        })
-      });
-    }).catch(() => {
-      if(evt.request.url.indexOf('.html') > -1){
-        return caches.match('fallback.html');
-      } 
-    })
-  );
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+      caches.match(event.request)
+          .then(() => {
+              return fetch(event.request) 
+                  .catch(() => caches.match('offline.html'))
+          })
+  )
 });
